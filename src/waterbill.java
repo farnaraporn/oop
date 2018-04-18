e25d228fe23ea178e16cf5448d7fda07b9e87fd6
Far <Far@DESKTOP-0E1N267>
2018-04-18 14:32:27 +0700

    start

---
+++ src/waterbill.java
@@ -0,0 +1,15 @@
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Far
+ */
+public class waterbill {
+   System.out.println("กด 1 ชำระบริการค่าน้ำ");
+}
