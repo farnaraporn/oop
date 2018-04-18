b9668259bb1d114b9289f75887ea8eb7cdbffe65
Far <Far@DESKTOP-0E1N267>
2018-04-18 16:44:21 +0700

    main

--- src/projectoop/Projectoop.java
+++ src/projectoop/Projectoop.java
@@ -1,38 +1,37 @@
-
 package projectoop;
 
 import java.util.Scanner;
+
 public class Projectoop {
 
     public static void main(String[] args) {
         int number;
-        do{
-        
-        System.out.println("กด 1 ชำระบริการค่าน้ำ");
-        System.out.println("กด 2 ชำระบริการค่าโทรสับ");
-        Scanner n = new Scanner(System.in); 
-        System.out.print("Enter number : ");
-        number = n.nextInt();
-    
-        if(number==2){
-        
-        //สร้าง oj
-        Enternumberphone ();
-        
-        }else{
-        
-        }
-        
-        
-        
-        }while(number!=1&&number!=2);  
-           
+        int select;
+        do {
+
+            System.out.println("กด 1 ชำระบริการค่าน้ำ");
+            System.out.println("กด 2 ชำระบริการค่าโทรสับ");
+            Scanner n = new Scanner(System.in);
+            System.out.print("Enter number : ");
+            number = n.nextInt();
+
+            if (number == 1) {
+
+                
+            } else {
+                System.out.println("แบบรายเดือน กด 1");
+                System.out.println("แบบเติมเงิน  กด 2 ");
+                Scanner s = new Scanner(System.in);
+                System.out.print("Enter number : ");
+                select = s.nextInt();
+                
+                Phone p = new Phone() {
+                };
+                p.Enternumberphone();
+            }
+
+        } while (number != 1 && number != 2);
+
     }
-    
-    
-    
-    
-    
-    
-    
+
 }
