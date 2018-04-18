cb7eacce78312f310bc87046fd88900b4a132495
Far <Far@DESKTOP-0E1N267>
2018-04-18 22:17:53 +0700

    money

--- src/projectoop/monthly.java
+++ src/projectoop/monthly.java
@@ -1,16 +1,14 @@
 
 package projectoop;
 
-import java.util.Scanner;
-
 public class monthly extends Phone implements showphone {
     monthly() {
-       System.out.println("----------------------"); 
+        
     }
     
     public  void show(){    
-    System.out.println("ยินดีให้บริการ");
-    System.out.println("==============================");
+     System.out.println("======ยินดีให้บริการ========");
+    
     }
     public void sphone(){
     super.Enternumberphone();
