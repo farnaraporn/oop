d0e37d868efe1b95d7247efdb9d504a839f019dd
Sunya <Sunya@172.19.53.76>
2018-04-19 21:23:10 +0700

    c

---
+++ src/projectoop/Customer.java
@@ -0,0 +1,13 @@
+
+package projectoop;
+
+public class Customer {
+    public void member () {
+       System.out.println("คุณมีหมายเลขสมาชิกไหม");
+       System.out.println("ถ้ามี กด 1");
+       System.out.println("ถ้าไม่มี กด 1"); 
+    }
+    
+    
+    
+}
